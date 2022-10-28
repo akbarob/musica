4ad58dcc515b4e62705495b02cd52637c776076b
Akbar <akbarbadmus07@gmail.com>
2022-10-28 13:08:26 +0100

    data

---
+++ src/assets/data.js
@@ -0,0 +1,18 @@
+export const countries = [
+  {
+    name: "Nigeria",
+    value: "NG",
+  },
+  {
+    name: "South Africa",
+    value: "SA",
+  },
+  {
+    name: "United Kingdom",
+    value: "GG",
+  },
+  {
+    name: "Usa",
+    value: "US",
+  },
+];
